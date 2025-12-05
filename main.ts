3641de2f942c59207cd867c32d825dbd2102a582
fabianjanker <fabian@janker.de>
2025-12-05 13:26:32 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        `)
+    music.playMelody("E B C5 A B G A F ", 10000)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        . # . # .
+        # . # . #
+        . # . # .
+        `)
+    music.playMelody("A F E F D G E F ", 10000)
+})
